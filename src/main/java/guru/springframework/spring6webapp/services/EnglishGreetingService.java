0a815b43774dd23ff7319f8ca95eb547c10c75ab
Kavitha Shivani <kavitha.shivani@iam-javelin.com>
2023-08-18 14:43:44 +1200

    Basic Spring Profile

---
+++ src/main/java/guru/springframework/spring6webapp/services/EnglishGreetingService.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6webapp.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("EN")
+@Service("i18NService")
+public class EnglishGreetingService implements GreetingService{
+    @Override
+    public String sayGreeting() {
+        return "Hello - EN";
+    }
+}
