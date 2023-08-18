b58ecb110026ab97e484e13996320becde8dbeaf
Kavitha Shivani <kavitha.shivani@iam-javelin.com>
2023-08-18 14:46:42 +1200

    Making a profile default.

--- src/main/java/guru/springframework/spring6webapp/services/EnglishGreetingService.java
+++ src/main/java/guru/springframework/spring6webapp/services/EnglishGreetingService.java
@@ -3,7 +3,7 @@ package guru.springframework.spring6webapp.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18NService")
 public class EnglishGreetingService implements GreetingService{
     @Override
